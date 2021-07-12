4d3f93ac405906bfef1ea6dacfbc497f63de1111
lderoose <leonard.deroose@gmail.com>
2021-07-12 18:22:43 +0200

    fix(data logs folder creation)

--- bin/install_with_conda.sh
+++ bin/install_with_conda.sh
@@ -3,5 +3,6 @@
 
 PYTHONPATH="/../../backtesting-trading-strategy"  # only replace this path
 cd $PYTHONPATH
+mkdir data logs
 awk -i inplace -v var="$PYTHONPATH" '{sub(/to_replace/, var, $0); print}' environment.yml 
 conda env create --file environment.yml 
